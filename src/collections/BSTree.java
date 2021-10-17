5c5b61899f7f501de477bdc87e6ec38ab1112477
alejandro945 <alejo8677@gmail.com>
2021-10-16 19:07:00 -0500

    BSS METHODS FIXES

--- src/collections/BSTree.java
+++ src/collections/BSTree.java
@@ -14,15 +14,15 @@ public class BSTree<K extends Comparable<K>, V extends Comparable<V>> implements
     private void inOrder(Node<K, V> current) {
         if (current != null) {
             inOrder(current.getLeft());
-            System.out.println(current.getValue());
+            System.out.println(current.getKey());
             inOrder(current.getRight());
         }
     }
 
     @Override
-    public V search(K key) {
+    public Node<K, V> search(K key) {
         if (root != null) {
-            return search(root, key).getValue();
+            return search(root, key);
         } else {
             return null;
         }
@@ -40,35 +40,47 @@ public class BSTree<K extends Comparable<K>, V extends Comparable<V>> implements
     }
 
     @Override
-    public K mininum() {
-        Node<K, V> temp = root;
-        while (temp.getLeft() != null) {
-            temp = temp.getLeft();
+    public K mininum(K key) {
+        Node<K, V> temp = search(key);
+        if (temp != null) {
+            while (temp.getLeft() != null) {
+                temp = temp.getLeft();
+            }
+            return temp.getKey();
+        } else {
+            return null;
         }
-        return temp.getKey();
     }
 
     @Override
-    public K maximun() {
-        Node<K, V> temp = root;
-        while (temp.getRight() != null) {
-            temp = temp.getRight();
+    public K maximun(K key) {
+        Node<K, V> temp = search(key);
+        if (temp != null) {
+            while (temp.getRight() != null) {
+                temp = temp.getRight();
+            }
+            return temp.getKey();
+        } else {
+            return null;
         }
-        return temp.getKey();
     }
 
     @Override
     public K sucessor(K key) {
-        Node<K, V> temp = (root != null) ? search(root, key) : null;
-        if (temp.getRight() != null) {
-            return mininum();
+        Node<K, V> temp = search(key);
+        if (temp != null) {
+            if (temp.getRight() != null) {
+                return mininum(temp.getRight().getKey());
+            }
+            Node<K, V> parent = temp.getParent();
+            while (parent != null && temp == parent.getRight()) {
+                temp = parent;
+                parent = parent.getParent();
+            }
+            return parent.getKey();
+        } else {
+            return null;
         }
-        Node<K, V> parent = temp.getParent();
-        while (parent != null && temp == parent.getRight()) {
-            temp = parent;
-            parent = parent.getParent();
-        }
-        return parent.getKey();
     }
 
     @Override
@@ -96,31 +108,33 @@ public class BSTree<K extends Comparable<K>, V extends Comparable<V>> implements
 
     @Override
     public void delete(K key) {
-        Node<K, V> toDelete = (root != null) ? search(root, key) : null;
-        Node<K, V> y = null;
-        Node<K, V> x = null;
-        if (toDelete.getLeft() == null || toDelete.getRight() == null) {
-            y = toDelete;
-        } else {
-            y = (root != null) ? search(root, sucessor(key)) : null;
-        }
-        if (y.getLeft() != null) {
-            x = y.getLeft();
-        } else {
-            x = y.getRight();
-        }
-        if (x != null) {
-            x.setParent(y.getParent());
-        }
-        if (y.getParent() == null) {
-            root = x;
-        } else if (y == y.getParent().getLeft()) {
-            y.getParent().setLeft(x);
-        } else {
-            y.getParent().setRight(x);
-        }
-        if (y != toDelete) {
-            toDelete.setKey(y.getKey());
+        Node<K, V> toDelete = search(key);
+        if (toDelete != null) {
+            Node<K, V> y = null;
+            Node<K, V> x = null;
+            if (toDelete.getLeft() == null || toDelete.getRight() == null) {
+                y = toDelete;
+            } else {
+                y = search(sucessor(key));
+            }
+            if (y.getLeft() != null) {
+                x = y.getLeft();
+            } else {
+                x = y.getRight();
+            }
+            if (x != null) {
+                x.setParent(y.getParent());
+            }
+            if (y.getParent() == null) {
+                root = x;
+            } else if (y == y.getParent().getLeft()) {
+                y.getParent().setLeft(x);
+            } else {
+                y.getParent().setRight(x);
+            }
+            if (y != toDelete) {
+                toDelete.setKey(y.getKey());
+            }
         }
     }
 }
--- src/collections/IBSTree.java
+++ src/collections/IBSTree.java
@@ -2,9 +2,9 @@ package collections;
 
 public interface IBSTree <K extends Comparable<K>, V>{
     void inOrder();
-    V search(K key);
-    K mininum();
-    K maximun();
+    Node<K,V> search(K key);
+    K mininum(K key);
+    K maximun(K key);
     K sucessor(K key);
     void insertion(K key, V value);
     void delete(K key);
